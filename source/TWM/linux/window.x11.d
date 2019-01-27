57629b3e620f483fadda50a7ba605bcd7fe4438e
Honeybunch <amt3824@g.rit.edu>
2019-01-27 09:13:30 -0800

    Fixing OSX and Linux TWM stubs

--- source/TWM/linux/window.x11.d
+++ source/TWM/linux/window.x11.d
@@ -2,4 +2,20 @@ module TWM.X11;
 
 version (linux):
 
+import std.stdint;
+
 public import TWM.common;
+
+bool ConstructWindow(uint16_t width, uint16_t height, Window* window)
+{
+  return false;
+}
+
+void PollEvents(const Window* window, Event* events, size_t eventCount)
+{
+
+}
+
+void ShutdownWindow(Window* window)
+{
+}
